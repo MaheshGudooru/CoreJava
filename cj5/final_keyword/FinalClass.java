58e6d7de1e2fb7c5c6acbb8abc36aa6d232cc2a5
dragon-slayer99 <guudurumahesh@gmail.com>
2026-01-23 22:01:26 +0530

    completed cj5 final keyword

---
+++ cj5/final_keyword/FinalClass.java
@@ -0,0 +1,35 @@
+package final_keyword;
+
+// final class cannot be extended
+public final class FinalClass {
+    // final variable - once assigned the value cannot be changed (becomes immutable)
+    public final double PI = 3.14;
+
+    public final void doSomething() { // final methods cannot be overridden but can be overloaded as below
+        System.out.println("doing something......");
+    }
+
+    public void doSomething(String s) {
+        System.out.println("the string is " + s);
+    }
+
+    static void main() {
+
+        FinalClass obj = new FinalClass();
+
+        System.out.println(obj.PI);
+
+//        obj.PI = 69; // this will work since PI is a final member of the class so essentially it is immutable
+
+        obj.doSomething();
+        obj.doSomething("String");
+    }
+
+}
+
+//class Test extends FinalClass { // This will not work since class is final
+//
+//    public void doSomething() { // This will not work since doSomething is final so it cannot be overridden
+//        System.out.println("doing new things....");
+//    }
+//}
